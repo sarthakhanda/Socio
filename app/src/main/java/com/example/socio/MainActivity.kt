bb5e69b8b84ebfee8fa6977a91d3c3167c34cb1d
sarthakhanda <sarthakhanda.work@gmail.com>
2021-05-25 01:43:48 +0530

    logout added

--- app/src/main/java/com/example/socio/MainActivity.kt
+++ app/src/main/java/com/example/socio/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.socio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.socio.daos.PostDao
 import com.example.socio.models.Post
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), IPostAdapter {
@@ -15,16 +20,73 @@ class MainActivity : AppCompatActivity(), IPostAdapter {
     private lateinit var postDao: PostDao
     private lateinit var adapter: PostAdapter
 
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
+
+    private var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener {
+            onAddButtonClicked()
+        }
+
+        fab1.setOnClickListener {
             val intent = Intent(this, CreatePostActivity::class.java)
             startActivity(intent)
         }
 
+        fab2.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         setUpRecyclerView()
+    }
+
+    private fun onAddButtonClicked() {
+        setVis(clicked)
+        setAnim(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVis(clicked: Boolean) {
+        if(!clicked) {
+            fab1.visibility = View.VISIBLE
+            fab2.visibility = View.VISIBLE
+        } else {
+            fab1.visibility = View.INVISIBLE
+            fab2.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setAnim(clicked: Boolean) {
+        if(!clicked) {
+            fab1.startAnimation(fromBottom)
+            fab2.startAnimation(fromBottom)
+            fab.startAnimation(rotateOpen)
+        } else {
+            fab1.startAnimation(toBottom)
+            fab2.startAnimation(toBottom)
+            fab.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setClickable(clicked: Boolean) {
+        if(!clicked) {
+            fab1.isClickable = true
+            fab2.isClickable = true
+        } else {
+            fab1.isClickable = false
+            fab2.isClickable = false
+        }
     }
 
     private fun setUpRecyclerView() {
